315320614fc6fc8e20e572c2f33d39a697b9c8c3
tiennv <tiennv597@gmail.com>
2020-04-21 00:30:49 +0700

    edit example grammar

--- models/grammar.js
+++ models/grammar.js
@@ -1,45 +1,61 @@
 // creat 2020/04/18
 
 var uuid = require('uuid');
-function Grammar(){
+function Grammar() {
 
-	var db=require("../config").db;	
+	var db = require("../config").db;
 
 	return {
 
-//add grammar to db
-		addGrammar : function (data, cb){
-			//var compatibleID=uuid.v4();	
-			var grammar={
-					provider:data.provider,
-					//id:compatibleID,
-  					level: data.level,
-					content: data.content,
-					mean:data.mean,
-					use:data.use,
-					note:data.note,
-					example : [
-						{
-							sentence: data.sentence, // sentence of  Japanese
-							vi: data.vi,             // Vietnamese
-							furigana:data.furigana 　// furigana of Japanese
-						}
-					 ],
+		//add grammar to db
+		addGrammar: function (data, cb) {
+			var compatibleID = uuid.v4();
+			var grammar = {
+				provider: data.provider,
+				grammar_id: compatibleID,
+				level: data.level,
+				content: data.content,
+				mean: data.mean,
+				use: data.use,
+				note: data.note,
 			};
 			//insert to colection grammars
-			db.grammars.insert(grammar,function(e, d){
-							cb(e,d);
-						});
+			db.grammars.insert(grammar, function (e, d) {
+				cb(e, d);
+			});
 		},
-//get grammar
-		getGrammar : function (cb){
+		//get grammar
+		getGrammar: function (cb) {
 			//get all grammars
-			db.grammars.find({},function(e, d){
-							cb(e,d);
-						});
+			db.grammars.find({}, function (e, d) {
+				cb(e, d);
+			});
 		},
+		////add example to db
+		addExample: function (data, cb) {
+			var compatibleID = uuid.v4();
+			var example = {
+				example_id: compatibleID,
+				grammar_id: data.grammar_id,
+				sentence: data.sentence,    // sentence of  Japanese
+				vi: data.vi,                // Vietnamese
+				furigana: data.furigana 　  // furigana of Japanese
+				,
+			};
+			//insert to colection examples
+			db.examples.insert(example, function (e, d) {
+				cb(e, d);
+			});
+		},
+		//get example with grammar_id
+		getExample: function (data,cb) {
+			//get all grammars
+			db.examples.find({grammar_id : data}, function (e, d) {
+				cb(e, d);
+			});
+		}
 
 	}
 }
 
-module.exports=Grammar;
+module.exports = Grammar;
