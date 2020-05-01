ca7ea71ba2895437fcff8b26da0a9ae2c4fc321f
tiennv <tiennv597@gmail.com>
2020-05-01 12:37:15 +0700

    test fb

--- config.js
+++ config.js
@@ -35,5 +35,5 @@ module.exports = {
 	facebookLoginEnabled: true,//setting login fb on
 	FACEBOOK_APP_ID : "534121453967696",
 	FACEBOOK_APP_SECRET: "969b28e61a37bb8314804705635c8781",
-	FACEBOOK_CALLBACK_DOMAIN: "https://localhost:3000/auth/facebook/callback"
+	FACEBOOK_CALLBACK_DOMAIN: "https://shinrojp-server.herokuapp.com/auth/facebook/callback"
 };
